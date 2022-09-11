ea11f55870653c05b3bf78ea47016712062099f5
loory <ece12akinci@gmail.com>
2022-09-11 23:21:22 +0300

    Asian Kitchen

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,61 +1,139 @@
-let newItem = ''
+const menu = [
+    {
+        id: 1,
+        title: "Tteokbokki",
+        category: "Korea",
+        price: 10.99,
+        img: "https://twoplaidaprons.com/wp-content/uploads/2020/09/tteokbokki-top-down-view-of-tteokbokki-in-a-bowl-500x500.jpg",
+        desc: `Spicy rice cakes, serving with fish cake.`,
+    },
+    {
+        id: 2,
+        title: "Chicken Ramen",
+        category: "Japan",
+        price: 7.99,
+        img: "https://www.forkknifeswoon.com/wp-content/uploads/2014/10/simple-homemade-chicken-ramen-fork-knife-swoon-01.jpg",
+        desc: `Chicken noodle soup, serving with vegetables such as soy bean, green onion. In an optional you can ask for egg. `,
+    },
+    {
+        id: 3,
+        title: "Bibimbap",
+        category: "Korea",
+        price: 8.99,
+        img: "https://dwellbymichelle.com/wp-content/uploads/2020/05/DWELL-bibimbap.jpg",
+        desc: `Boiling vegetables, serving with special hot sauce`,
+    },
+    {
+        id: 4,
+        title: "Dan Dan Mian",
+        category: "China",
+        price: 5.99,
+        img: "https://www.savingdessert.com/wp-content/uploads/2019/02/Dan-Dan-Noodles-10.jpg",
+        desc: `Dan dan noodle, serving with green onion `,
+    },
+    {
+        id: 5,
+        title: "Yangzhou Fried Rice",
+        category: "China",
+        price: 12.99,
+        img: "https://salu-salo.com/wp-content/uploads/2013/02/Yangzhou-Fried-Rice1.jpg",
+        desc: `Yangzhou style fried rice, serving with bean and pickles `,
+    },
+    {
+        id: 6,
+        title: "Onigiri",
+        category: "Japan",
+        price: 9.99,
+        img: "https://www.manusmenu.com/wp-content/uploads/2017/08/Onigiri-3-1-of-1.jpg",
+        desc: `Rice Sandwich, serving with soy sauce`,
+    },
+    {
+        id: 7,
+        title: "Jajangmyeon",
+        category: "Korea",
+        price: 15.99,
+        img: "https://www.curiouscuisiniere.com/wp-content/uploads/2020/04/Jajangmyeon-Korean-Noodles-in-Black-Bean-Sauce5.1200H-720x540.jpg",
+        desc: `Black bean sauce noodle, serving with green onion `,
+    },
+    {
+        id: 8,
+        title: "Ma Yi Shang Shu",
+        category: "China",
+        price: 12.99,
+        img: "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/F688C2F6-86EC-46C4-B9C7-A6BA01DF7437/Derivates/32E3E72A-F786-406D-AF7F-B30980A9AC6C.jpg",
+        desc: `Hot pepper sauce noodle, serving with soy bean and onion`,
+    },
+    {
+        id: 9,
+        title: "Doroyaki",
+        category: "Japan",
+        price: 3.99,
+        img: "https://www.justonecookbook.com/wp-content/uploads/2011/10/Dorayaki-New-500x400.jpg",
+        desc: `Red bean paste dessert, serving with honey.`,
+    },
+];
 
-//Selectors
-let list = document.getElementById('list')
-let inputField = document.getElementById('task')
-let addTodo = document.getElementById('liveToastBtn')
+/*containers*/
+let container = document.getElementById('menu-items')
+let button = document.getElementById('button-container')
 
-//Toast messages
-let errorMsg = new bootstrap.Toast(document.getElementById('toastError'))
-let successMsg = new bootstrap.Toast(document.getElementById('toastSuccess'))
+/*categories of foods*/
+let uniqueCategories = [...new Set(menu.map(x => x.category))]
+uniqueCategories.unshift('All')
 
-//Event listeners
-inputField.addEventListener('change',  inputListener)
-addTodo.addEventListener('click', function() { addItem(newItem, list) })
-list.addEventListener('click', function(e){ listenList(e) })
+/*Render foods*/
+render(container, foodContainer, menu)
 
-//Remove item from todolist
-function removeItem(e) {
-    e.parentNode.remove()
-}
+/*Render buttons*/
+render(button, buttonContainer, uniqueCategories)
 
-//Equalize global variable 'newItem' to input value
-function inputListener() {
-    newItem = this.value
-}
+let buttons = document.querySelectorAll('button')
 
-//Listen list elements
-//If target is xMark, then remove item. If target is list item itself, then add checked class
-function listenList(e) {
-    let element = e.target
+/*If category id is All, then show all foods,
+    else render that category*/
+buttons.forEach(btn => {
 
-    if (element.classList.contains('xmark')) {
-        removeItem(element)
-    }else{
-        if(element.matches('.checked')){
-            element.classList.remove('checked')
-        }else{
-            element.classList.add('checked')
+    btn.addEventListener('click', event => {
+        let category = event.target.getAttribute('data-id')
+
+        if(category === "All"){
+            container.innerHTML = ''
+            render(container, foodContainer, menu)
         }
-    }
+        else{
+            container.innerHTML = ''
+            let newMenu = menu.filter(item => item.category === category)
+            render(container, foodContainer, newMenu)
+        }
+    });
+});
+
+/*Return one food item*/
+function foodContainer(menuItem) {
+    return  `
+    <div class="menu-items col-lg-6 col-sm-12">
+        <img src="${menuItem.img}" alt="${menuItem.title}" class="photo">
+        <div class="menu-info">
+            <div class="menu-title">
+                <h4>${menuItem.title}</h4>
+                <h4 class="price">${menuItem.price}</h4>
+            </div>
+            <div class="menu-text">${menuItem.desc}</div>
+        </div>
+    </div>
+    `
 }
 
-//Add item to list, change input item, display toast messages
-function addItem(input, list) {
-    if(!isEmpty(input)) {
-        let listElement = document.createElement('li')
-        listElement.innerHTML = input + `<img class="xmark" src="assets/xmark.svg">`
-        list.append(listElement)
-
-        newItem = ''
-        inputField.value = ''
-        successMsg.show()
-    }else{
-        errorMsg.show()
-    }
+/*Return one button*/
+function buttonContainer(buttonItem) {
+    return `
+        <button class="btn btn-outline-dark btn-item" data-id = "${buttonItem}">${buttonItem}</button>
+    `
 }
 
-//Check if input is empty
-function isEmpty (input) {
-    return !input.trim().length
+/*Render containers based on container, callback funtion and specified array*/
+function render(container, callBackFunc, array) {
+    for(let i = 0; i < array.length; i++) {
+        container.insertAdjacentHTML('beforeend', callBackFunc(array[i]))
+    }
 }
