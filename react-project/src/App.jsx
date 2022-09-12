9d2730a4ba3e246afc04be71c8c70d502c69b7c3
loory <ece12akinci@gmail.com>
2022-09-12 22:57:47 +0300

    React Homework 1

---
+++ react-project/src/App.jsx
@@ -0,0 +1,12 @@
+import data from './data.js'
+import './App.css'
+
+function App() {
+    console.log(data())
+  return (
+    <div className="App">
+        Dummy
+    </div>
+  )
+}
+export default App
